e21aa18e9929a881524649857f7d543eaf4b2829
Sarangi Bendre <sarangibendre655@gmail.com>
2026-05-14 18:08:07 +0530

    Add files via upload

--- test_model_run.py
+++ test_model_run.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-model = YOLO("runs/detect/train/weights/best.pt")
+model = YOLO("model/best.pt")
 
 results = model.predict(
     source="D:/CleanRiverAI/dataset/test/images/",
